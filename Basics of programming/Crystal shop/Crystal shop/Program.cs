631bedf1ea43a2cb90e1d53615aa2fb530d044b0
ivane <ivan.ega4ev@yandex.ru>
2023-01-23 18:05:32 +0300

    replace int crystalCanBuy operator to crystalCanBuy

--- Basics of programming/Crystal shop/Crystal shop/Program.cs
+++ Basics of programming/Crystal shop/Crystal shop/Program.cs
@@ -2,15 +2,11 @@
 int goldCount = Convert.ToInt32(Console.ReadLine());
 
 int crystalPrice = 17;
-int crystalCanBuy = goldCount / crystalPrice;
+int crystalsCanBuy = goldCount / crystalPrice;
 
-Console.Write($"Вы можете купить {crystalCanBuy} кристаллов(а), какое количество хотите приобрести: ");
+Console.Write($"Вы можете купить {crystalsCanBuy} кристаллов(а), какое количество хотите приобрести: ");
 int crystalToBuy = Convert.ToInt32(Console.ReadLine());
 
-int remainder = goldCount - (crystalToBuy * crystalPrice);
+int remainder = goldCount - crystalToBuy * crystalPrice;
 
-Console.WriteLine($"Вы приобрели {crystalToBuy} кристаллов. Ваш отстаток - {remainder} золота"); 
-
-
-
-
+Console.WriteLine($"Вы приобрели {crystalToBuy} кристаллов. Ваш отстаток - {remainder} золота");
