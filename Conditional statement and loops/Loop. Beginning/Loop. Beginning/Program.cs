4a8210bfa23bff1ade425492240399f2c2e2d9d4
ivane <ivan.ega4ev@yandex.ru>
2023-01-25 22:58:11 +0300

    Add new theme

---
+++ Conditional statement and loops/Loop. Beginning/Loop. Beginning/Program.cs
@@ -0,0 +1,13 @@
+﻿//При помощи циклов вы можете повторять один и тот же код множество раз.
+//Напишите простейшую программу, которая выводит указанное(установленное) пользователем сообщение заданное количество раз.
+//Количество повторов также должен ввести пользователь.
+
+Console.Write("Enter tour message: ");
+string message = Console.ReadLine();
+Console.Write("How many times repeat this message: ");
+int timesToReapeat = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < timesToReapeat; i++)
+{
+    Console.WriteLine(message);
+}
