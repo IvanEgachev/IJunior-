eebdb5e5508c0bfbf58e618afafc71f3f77c9345
ivane <ivan.ega4ev@yandex.ru>
2023-02-16 22:23:59 +0300

    Sequence  delete all comments

--- Conditional statement and loops/Sequence/Sequence/Program.cs
+++ Conditional statement and loops/Sequence/Sequence/Program.cs
@@ -1,16 +1,10 @@
-﻿//Нужно написать программу (используя циклы, обязательно пояснить выбор вашего цикла), 
-//чтобы она выводила следующую последовательность 5 12 19 26 33 40 47 54 61 68 75 82 89 96
-//Нужны переменные для обозначения чисел в условии цикла.
-
-int firstSequenceNumber = 5;
+﻿int firstSequenceNumber = 5;
 int secondSequenceNumber = 12;
 int lastSequenceNumber = 96;
 
 int step = secondSequenceNumber - firstSequenceNumber;
 
-for (int currentNumber = firstSequenceNumber; currentNumber <= lastSequenceNumber; currentNumber+= step)
+for (int i = firstSequenceNumber; i <= lastSequenceNumber; i+= step)
 {
-    Console.Write(currentNumber+" ");
+    Console.Write(i+" ");
 }
-
-//Пояснение: был выбран цикла "for", поскольку данные выводятся с четко определенным шагом, задано начальное и конечное значение. 
