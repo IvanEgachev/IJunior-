6e59350eb7c717fb9a08bdee26dd0b1bd5462ac7
ivane <ivan.ega4ev@yandex.ru>
2023-02-16 22:15:16 +0300

    loop beginning change

--- Conditional statement and loops/Loop. Beginning/Loop. Beginning/Program.cs
+++ Conditional statement and loops/Loop. Beginning/Loop. Beginning/Program.cs
@@ -1,8 +1,4 @@
-﻿//При помощи циклов вы можете повторять один и тот же код множество раз.
-//Напишите простейшую программу, которая выводит указанное(установленное) пользователем сообщение заданное количество раз.
-//Количество повторов также должен ввести пользователь.
-
-Console.Write("Enter tour message: ");
+﻿Console.Write("Enter tour message: ");
 string message = Console.ReadLine();
 
 Console.Write("How many times repeat this message: ");
