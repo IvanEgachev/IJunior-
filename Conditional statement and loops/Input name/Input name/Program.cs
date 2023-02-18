ad49a88e40ea9cca741830a358e29ed6ccb281dc
ivane <ivan.ega4ev@yandex.ru>
2023-02-18 21:44:49 +0300

    input name fix

--- Conditional statement and loops/Input name/Input name/Program.cs
+++ Conditional statement and loops/Input name/Input name/Program.cs
@@ -2,19 +2,19 @@
 string inputName = Console.ReadLine();
 
 Console.Write("Введите символ для отрисовки рамки: ");
-string inputDrawSymbol = Console.ReadLine();
+char inputDrawSymbol = Convert.ToChar(Console.ReadLine());
 
-inputName = inputDrawSymbol + inputName+ inputDrawSymbol;
+string inBorderName = inputDrawSymbol + inputName+ inputDrawSymbol;
 int drawBordersCount = 2;
 
 for (int i = 0; i < drawBordersCount; i++)
 {
 
-    for (int j = 0; j < inputName.Length; j++)
+    for (int j = 0; j < inBorderName.Length; j++)
     {
         Console.Write(inputDrawSymbol);
     }
 
     if (i == 0)
-        Console.WriteLine("\n" + inputName);
+        Console.WriteLine("\n" + inBorderName);
 }
