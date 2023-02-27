a05d4333417e52751aaf7e3a288afa0c96f30fd2
ivane <ivan.ega4ev@yandex.ru>
2023-02-27 22:32:02 +0300

    input name v.2

---
+++ Conditional statement and loops/Input name v.2/Input name v.2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите Ваше имя: ");
+string inputName = Console.ReadLine();
+
+Console.Write("Введите символ для отрисовки рамки: ");
+char inputDrawSymbol = Convert.ToChar(Console.ReadLine());
+
+for (int i = 0; i < inputName.Length + 2; i++)
+{
+    Console.Write(inputDrawSymbol);
+}
+
+Console.WriteLine("\n"+ inputDrawSymbol + inputName + inputDrawSymbol);
+
+for (int i = 0; i < inputName.Length + 2; i++)
+{
+    Console.Write(inputDrawSymbol);
+}
