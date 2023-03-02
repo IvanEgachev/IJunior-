e4acf6110b6e8f59ec7ce8a2f4d4b05fba12bac8
ivane <ivan.ega4ev@yandex.ru>
2023-03-02 21:38:46 +0300

    power of two add

---
+++ Conditional statement and loops/Power of two/Power of two/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+
+long number = random.Next();
+Console.WriteLine("Исходное число "+number);
+
+int powerOfTwo = 0;
+long numberToPower = 1;
+
+while (numberToPower < number)
+{
+    numberToPower *= 2;
+    powerOfTwo++; 
+}
+   
+Console.WriteLine($"Степень двойки, превышающее исходное число  {powerOfTwo} ({numberToPower})");
