afdba2036e23af6b22bcca73f45d487e6099266c
ivane <ivan.ega4ev@yandex.ru>
2023-03-03 21:08:49 +0300

    added boundary variables

--- Conditional statement and loops/Multiples/Multiples/Program.cs
+++ Conditional statement and loops/Multiples/Multiples/Program.cs
@@ -1,17 +1,18 @@
 ﻿Random random = new Random();
 
-int number = random.Next(1, 28);
-int last3DigitNumber = 999;
+int minInputNumber = 1;
+int maxInputNumber = 27;
+
+int inputNumber = random.Next(minInputNumber, maxInputNumber + 1);
+int maxRangeNumber = 999;
 int multiplesNumbers = 0;
 
-Console.WriteLine("Выбранное число из диапозона:" + number);
-
-for (int i = number; i <= last3DigitNumber; i+=number)
+for (int i = inputNumber; i <= maxRangeNumber; i+= inputNumber)
 {
-    if ((int)Math.Log10(i) + 1 == 3)
+    if (i >= 100)
     {
         multiplesNumbers++;
     }
 }
 
-Console.WriteLine($"Количество чисел кратных {number} - {multiplesNumbers}");
+Console.WriteLine($"Количество чисел кратных {inputNumber} - {multiplesNumbers}");
