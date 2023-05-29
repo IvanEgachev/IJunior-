3633d080f4f0a128c9864e45618f1ccf7c2e2dfb
ivane <ivan.ega4ev@yandex.ru>
2023-05-29 20:05:33 +0300

    Local max add

---
+++ Arrays/Local max/Local max/Program.cs
@@ -0,0 +1,40 @@
+﻿Random rand =  new Random();
+
+int[,] matrix;
+
+Console.Write("Количество строк в матрице ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Количество столбцов в матрице ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+matrix = new int[rows, columns];
+int secondRow = 1;
+int secondRowSum = 0;
+
+int firstColumn = 0;
+int firstColumnMultiplication = 1;
+
+for (int i = 0; i < rows; i++)
+{
+
+	for (int j = 0; j < columns; j++)
+	{
+		matrix[i, j] = rand.Next(0, 100);
+		Console.Write(Convert.ToString(matrix[i, j])+'\t');
+		
+		if (i == secondRow)
+		{
+			secondRowSum += matrix[secondRow, j];
+		}
+	}
+
+	Console.WriteLine();
+
+    firstColumnMultiplication *= matrix[i, firstColumn];
+}
+
+Console.WriteLine("Сумма второй строки:"+ secondRowSum + ", произведение первого столбца: " + firstColumnMultiplication);
+
+
+
