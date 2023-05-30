b7f2b5a745149141e645492a8afe16a1a8a0dd5c
ivane <ivan.ega4ev@yandex.ru>
2023-05-30 20:37:55 +0300

    add DynamicArray

---
+++ Arrays/DynamicArray/DynamicArray/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] array = new int[0];
+
+string userInput;
+bool isExit = false;
+
+while (!isExit)
+{
+    Console.Write("Введите число или команду: ");
+    userInput = Console.ReadLine();
+
+    switch (userInput)
+    {
+        case "sum":
+            Console.WriteLine(array.Sum());
+            break;
+
+        case "exit":
+            isExit = true;
+            break;
+
+        default:
+            int tempInputNumber; 
+
+            if (int.TryParse(userInput, out tempInputNumber))
+            {
+                int[] tempArray = new int[array.Length + 1];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    tempArray[i] = array[i];
+                }
+
+                array = tempArray;
+                array[array.Length - 1] = tempInputNumber;
+            }
+            else
+            {
+                Console.WriteLine("Введено некорректоное значение");
+            }        
+            break;
+
+            Console.ReadKey();
+            Console.Clear();
+    }
+}
