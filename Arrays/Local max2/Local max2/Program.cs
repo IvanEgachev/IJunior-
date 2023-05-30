30cd2c25c1c64bc2b303a850ff4a8de5184d8092
ivane <ivan.ega4ev@yandex.ru>
2023-05-30 19:58:59 +0300

    add local max 2

---
+++ Arrays/Local max2/Local max2/Program.cs
@@ -0,0 +1,39 @@
+﻿Random rand = new Random();
+
+int[] array;
+
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+array = new int[arrayLength];
+
+for (int i = 0; i < arrayLength; i++)
+{
+    array[i] = rand.Next(0, 100);
+    Console.Write(array[i] + " ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < arrayLength; i++)
+{
+    if (i == 0)
+    {
+        if (array[i] > array[i+1])
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    else if (i == arrayLength - 1)
+    {
+        if (array[i] > array[i - 1])
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    else
+    {
+        if (array[i] > array[i + 1] && array[i] > array[i - 1])
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
