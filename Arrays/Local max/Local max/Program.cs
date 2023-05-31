4aac682434dccb0c9b334a82ba92718babd1f13f
ivane <ivan.ega4ev@yandex.ru>
2023-05-31 19:48:31 +0300

    Local max. One cycle is divided into three

--- Arrays/Local max/Local max/Program.cs
+++ Arrays/Local max/Local max/Program.cs
@@ -1,4 +1,4 @@
-﻿Random rand =  new Random();
+﻿Random random =  new Random();
 
 int[,] matrix;
 
@@ -9,28 +9,33 @@ Console.Write("Количество столбцов в матрице ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
 matrix = new int[rows, columns];
+
 int secondRow = 1;
 int secondRowSum = 0;
 
 int firstColumn = 0;
 int firstColumnMultiplication = 1;
 
+int minMatrixNumber = 0;
+int maxMatrixNumber = 100;
+
 for (int i = 0; i < rows; i++)
 {
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = random.Next(minMatrixNumber, maxMatrixNumber);
+        Console.Write(Convert.ToString(matrix[i, j]) + '\t');
+    }
+    Console.WriteLine();
+}
 
-	for (int j = 0; j < columns; j++)
-	{
-		matrix[i, j] = rand.Next(0, 100);
-		Console.Write(Convert.ToString(matrix[i, j])+'\t');
-		
-		if (i == secondRow)
-		{
-			secondRowSum += matrix[secondRow, j];
-		}
-	}
+for (int i = 0; i < columns; i++)
+{
+        secondRowSum += matrix[secondRow, i];
+}
 
-	Console.WriteLine();
-
+for (int i = 0; i < rows; i++)
+{
     firstColumnMultiplication *= matrix[i, firstColumn];
 }
 
