f70a1ce76ba6d8cc937b4e8c6d825107dd112183
ivane <ivan.ega4ev@yandex.ru>
2023-06-05 15:55:07 +0300

    Max element add params

--- Arrays/Max Element/Max Element/Program.cs
+++ Arrays/Max Element/Max Element/Program.cs
@@ -3,13 +3,17 @@
 int[,] matrix = new int[10, 10];
 int maxElement = int.MinValue;
 
+int minRandomElement = 0;
+int maxRandomElemnt = 20;
+
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i, j] = rand.Next(0, 20);
+        matrix[i, j] = rand.Next(minRandomElement, maxRandomElemnt);
         Console.Write(Convert.ToString(matrix[i, j]) + '\t');
     }
+
     Console.WriteLine();
 }
 
