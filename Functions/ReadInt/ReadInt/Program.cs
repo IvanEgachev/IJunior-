a941fec2cccbe6177916568f25a56441dbb0dbad
ivane <ivan.ega4ev@yandex.ru>
2023-06-15 16:39:19 +0300

    add readInt

---
+++ Functions/ReadInt/ReadInt/Program.cs
@@ -0,0 +1,22 @@
+﻿static int ReadInt()
+{
+    int number;
+
+    while (true)
+    {
+        Console.WriteLine("Введите число: ");
+
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Это не число");
+        }
+    }
+}
+
+int number = ReadInt();
+
+Console.WriteLine(number);
