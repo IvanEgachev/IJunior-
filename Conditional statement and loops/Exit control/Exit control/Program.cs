dd825799eaea4add935237912faf0fac4d59a5e8
ivane <ivan.ega4ev@yandex.ru>
2023-12-02 19:31:03 +0300

    add userInput

--- Conditional statement and loops/Exit control/Exit control/Program.cs
+++ Conditional statement and loops/Exit control/Exit control/Program.cs
@@ -1,3 +1,11 @@
-while(Console.ReadLine() != "exit"){}
+const string exitCommand = "exit";
+
+Console.WriteLine("Введите что-нибудь");
+string userInput = ""; 
+
+while(userInput != exitCommand)
+    {
+        userInput = Console.ReadLine();
+    }
 
 Console.WriteLine("Произошел выход из цикла");
