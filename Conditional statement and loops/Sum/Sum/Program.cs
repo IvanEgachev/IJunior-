9998098658f10b4b2eb7c591da6bf1b1f59f6bbe
ivane <ivan.ega4ev@yandex.ru>
2023-12-12 21:40:37 +0300

    remove else

--- Conditional statement and loops/Sum/Sum/Program.cs
+++ Conditional statement and loops/Sum/Sum/Program.cs
@@ -12,16 +12,12 @@ Console.Write("Последовательность чисел: ");
 
 for (int i = number; i > 0; i--)
 {
-    if(i % multipleNumber1 == 0)
+    if(i % multipleNumber1 == 0 || i % multipleNumber2 == 0)
     {
         sum += i;
         Console.Write(i+" ");
     }
-    else if (i % multipleNumber2 == 0)
-    {
-        sum += i;
-        Console.Write(i + " ");
-    }
+   
 }
 
 Console.WriteLine("\nСумма чисел: "+sum);
