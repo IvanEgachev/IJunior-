36aca9a0f392e12c42f20d1bf3e253747fb487ba
ivane <ivan.ega4ev@yandex.ru>
2024-02-20 21:50:47 +0300

    while replace to for

--- Conditional statement and loops/Power of two/Power of two/Program.cs
+++ Conditional statement and loops/Power of two/Power of two/Program.cs
@@ -1,16 +1,15 @@
 ﻿Random random = new Random();
 
-long inputNumber = random.Next();
+long inputNumber = random.Next(0, 100000);
 Console.WriteLine("Исходное число "+ inputNumber);
 
 int numberToPower = 2;
 int powerOfNumber = 0;
 long result = 1;
 
-while (result <= inputNumber)
+for (; result <= inputNumber; powerOfNumber++)
 {
     result *= numberToPower;
-    powerOfNumber++; 
 }
-   
+
 Console.WriteLine($"Степень двойки, превышающее исходное число  {powerOfNumber} ({result})");
