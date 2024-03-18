031120b1783ad46319d98b4142e2e5c5dee66f46
ivane <ivan.ega4ev@yandex.ru>
2024-03-18 20:17:34 +0300

    add ArrayStrings

---
+++ Arrays/ArrayStrings/ArrayStrings/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ArrayStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,] { { 13, 26, 17 }, { 5, 12, 34 }, { 54, 77, 36 }, { 1, 2, 3 } };
+
+            int line = 2;
+            int column = 1;
+
+            int lineSum = 0;
+            int columnMultiple = 1;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == column - 1)
+                    {
+                        columnMultiple *= matrix[i, j];
+                    }
+                    if (i == line - 1)
+                    {
+                        lineSum += matrix[i, j];
+                    }
+
+                    Console.Write(matrix[i, j] + "\t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Результат:\n Произведение {column} столбца: {columnMultiple}  \n Сумма {line} строки: {lineSum}");
+        }
+    }
+}
