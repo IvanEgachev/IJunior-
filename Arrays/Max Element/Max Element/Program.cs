2b70b4f9842704876c2e98f6be9c9d8bbaad9a46
ivane <ivan.ega4ev@yandex.ru>
2024-04-01 16:46:01 +0300

    add namespacce, class Program

--- Arrays/Max Element/Max Element/Program.cs
+++ Arrays/Max Element/Max Element/Program.cs
@@ -1,54 +1,64 @@
-﻿Random random = new Random();
-
-int[,] matrix = new int[10, 10];
-int maxElement = int.MinValue;
-
-int elementToReplace = 0;
-
-int minRandomElement = 0;
-int maxRandomElemnt = 20;
-
-for (int i = 0; i < matrix.GetLength(0); i++)
+﻿
+namespace MaxElement
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    internal class Program
     {
-        matrix[i, j] = random.Next(minRandomElement, maxRandomElemnt);
-        Console.Write(Convert.ToString(matrix[i, j]) + '\t');
-    }
-
-    Console.WriteLine();
-}
-
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        if (matrix[i, j] > maxElement)
+        static void Main(string[] args)
         {
-            maxElement = matrix[i, j];
+            Random random = new Random();
+
+            int[,] matrix = new int[10, 10];
+            int maxElement = int.MinValue;
+
+            int elementToReplace = 0;
+
+            int minRandomElement = 0;
+            int maxRandomElemnt = 1000;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(minRandomElement, maxRandomElemnt);
+                    Console.Write(Convert.ToString(matrix[i, j]) + '\t');
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > maxElement)
+                    {
+                        maxElement = matrix[i, j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == maxElement)
+                    {
+                        matrix[i, j] = elementToReplace;
+                    }
+                }
+            }
+
+            Console.WriteLine("Максимальный элемент матрицы: " + maxElement);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(Convert.ToString(matrix[i, j]) + '\t');
+                }
+
+                Console.WriteLine();
+            }
         }
     }
-}
-
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        if (matrix[i, j] == maxElement)
-        {
-            matrix[i, j] = elementToReplace;
-        }
-    }
-}
-
-Console.WriteLine("Максимальный элемент матрицы: " + maxElement);
-
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write(Convert.ToString(matrix[i, j]) + '\t');
-    }
-
-    Console.WriteLine();
 }
