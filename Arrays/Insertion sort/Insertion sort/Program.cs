a3e507df96e592aeeb919a8b86d2fa2aa5e1bbd9
ivane <ivan.ega4ev@yandex.ru>
2024-04-02 20:49:43 +0300

    Insertion sort

--- Arrays/Insertion sort/Insertion sort/Program.cs
+++ Arrays/Insertion sort/Insertion sort/Program.cs
@@ -1,40 +1,46 @@
-﻿Random random = new Random();
-
-int[] numbers;
-int arrayLength;
-
-int minArrayNumber = 0;
-int maxArrayNumber = 100;
-
-Console.Write("Введите размерность массива: ");
-arrayLength = Convert.ToInt32(Console.ReadLine());
-
-numbers = new int[arrayLength];
-
-Console.WriteLine("Изначальный вид массива:");
-
-for (int i = 0; i < numbers.Length; i++)
+﻿internal class Program
 {
-    numbers[i] = random.Next(minArrayNumber, maxArrayNumber);
-    Console.Write(numbers[i]+ " ");
-}
-
-for (int i = 1; i < numbers.Length; i++)
-{
-    for (int j = i; j > 0 ; j--)
+    private static void Main(string[] args)
     {
-        if (numbers[j] < numbers[j-1])
+        Random random = new Random();
+
+        int[] numbers;
+        int arrayLength;
+
+        int minArrayNumber = 0;
+        int maxArrayNumber = 100;
+
+        Console.Write("Введите размерность массива: ");
+        arrayLength = Convert.ToInt32(Console.ReadLine());
+
+        numbers = new int[arrayLength];
+
+        Console.WriteLine("Изначальный вид массива:");
+
+        for (int i = 0; i < numbers.Length; i++)
         {
-            int tempVariable = numbers[j];
-            numbers[j] = numbers[j-1];
-            numbers[j-1] = tempVariable;
+            numbers[i] = random.Next(minArrayNumber, maxArrayNumber);
+            Console.Write(numbers[i] + " ");
+        }
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                if (numbers[j] < numbers[j - 1])
+                {
+                    int tempVariable = numbers[j];
+                    numbers[j] = numbers[j - 1];
+                    numbers[j - 1] = tempVariable;
+                }
+            }
+        }
+
+        Console.WriteLine("\nОтсортированный массив:");
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
         }
     }
-}
-
-Console.WriteLine("\nОтсортированный массив:");
-
-for (int i = 0; i < numbers.Length; i++)
-{
-    Console.Write(numbers[i] + " ");
 }
