b1441d97e041cc2c683d6a284a37bc4a5ef85de7
ivane <ivan.ega4ev@yandex.ru>
2024-04-02 19:49:34 +0300

    Max Subarray

--- Arrays/Max subarray/Max subarray/Program.cs
+++ Arrays/Max subarray/Max subarray/Program.cs
@@ -1,51 +1,61 @@
-﻿Random random = new Random();
-
-int[] array;
-int arrayLength = 0;
-
-int subArrayLength = 0;
-int maxSubArrayLength = 0;
-int subarrayNumber = 0;
-
-int minArrayNumber = 0;
-int maxArrayNumber = 5;
-
-Console.Write("Введите размерность массива ");
-
-if (int.TryParse(Console.ReadLine(), out arrayLength))
+﻿namespace MaxSubarray
 {
-    array = new int[arrayLength];
-
-    for (int i = 0; i < arrayLength; i++)
+    class Program
     {
-        array[i] = random.Next(minArrayNumber, maxArrayNumber);
-        Console.Write(array[i] + " ");
-    }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
 
-    if (array.Length != 0)
-	{
-		subArrayLength = 1;
-        maxSubArrayLength = subArrayLength;
-        subarrayNumber = array[0];
-	}
+            int[] array;
+            int arrayLength = 0;
 
-	for (int i = 1; i < array.Length; i++)
-	{
-		if (array[i] == array[i-1])
-		{
-			subArrayLength++;
+            int subArrayLength = 0;
+            int maxSubArrayLength = 0;
+            int subarrayNumber = 0;
 
-            if (subArrayLength >= maxSubArrayLength)
+            int minArrayNumber = 0;
+            int maxArrayNumber = 5;
+
+            Console.Write("Введите размерность массива ");
+
+            if (int.TryParse(Console.ReadLine(), out arrayLength))
             {
-                maxSubArrayLength = subArrayLength;
-                subarrayNumber = array[i];
+                array = new int[arrayLength];
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    array[i] = random.Next(minArrayNumber, maxArrayNumber);
+                    Console.Write(array[i] + " ");
+                }
+
+                if (array.Length != 0)
+                {
+                    subArrayLength = 1;
+                    maxSubArrayLength = subArrayLength;
+                    subarrayNumber = array[0];
+                }
+
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if (array[i] == array[i - 1])
+                    {
+                        subArrayLength++;
+
+                        if (subArrayLength >= maxSubArrayLength)
+                        {
+                            maxSubArrayLength = subArrayLength;
+                            subarrayNumber = array[i];
+                        }
+                    }
+                    else
+                    {
+                        subArrayLength = 1;
+                    }
+                }
+
+                Console.WriteLine($"\nДлина  самого большого подмассива: {maxSubArrayLength}, Число подмассива: {subarrayNumber}.");
             }
         }
-		else
-		{
-            subArrayLength = 1;
-		}
-	}
+    }
 
-	Console.WriteLine($"\nДлина  самого большого подмассива: {maxSubArrayLength}, Число подмассива: {subarrayNumber}.");
 }
