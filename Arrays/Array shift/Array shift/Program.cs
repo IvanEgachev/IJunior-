377d8f9c7776479558523d75c8412c138eda6e65
ivane <ivan.ega4ev@yandex.ru>
2024-04-03 21:16:37 +0300

    ArrayShift

--- Arrays/Array shift/Array shift/Program.cs
+++ Arrays/Array shift/Array shift/Program.cs
@@ -1,47 +1,53 @@
-﻿            Random random = new Random();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
 
-            int[] array;
-            int arrayLength;
-                   
-            int userInputShift;
-            int shift;
+        int[] array;
+        int arrayLength;
 
-            int minArrayNumber = 0;
-            int maxArrayNumber = 9;
+        int userInputShift;
+        int shift;
 
-            Console.Write("Введите размерность массива: ");
-            arrayLength = Convert.ToInt32(Console.ReadLine());
-            
-            array = new int[arrayLength];
+        int minArrayNumber = 0;
+        int maxArrayNumber = 9;
 
-            Console.WriteLine("Исходный массив: ");
+        Console.Write("Введите размерность массива: ");
+        arrayLength = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < array.Length; i++)
+        array = new int[arrayLength];
+
+        Console.WriteLine("Исходный массив: ");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(minArrayNumber, maxArrayNumber);
+            Console.Write(array[i] + " ");
+        }
+
+        Console.Write("\nНа сколько позиций сдвинуть массив влево: ");
+        userInputShift = Convert.ToInt32(Console.ReadLine());
+
+        shift = userInputShift % array.Length;
+
+        int tempNumber;
+
+        for (int i = 0; i < shift; i++)
+        {
+            tempNumber = array[0];
+
+            for (int j = 0; j < array.Length - 1; j++)
             {
-                  array[i] = random.Next(minArrayNumber, maxArrayNumber);
-                  Console.Write(array[i] + " ");
+                array[j] = array[j + 1];
             }
 
-            Console.Write("\nНа сколько позиций сдвинуть массив влево: ");
-            userInputShift = Convert.ToInt32(Console.ReadLine());
+            array[array.Length - 1] = tempNumber;
+        }
 
-            shift = userInputShift % array.Length;
-
-            int tempNumber;
-
-            for (int i = 0; i < shift; i++)
-            {
-                tempNumber = array[0];
-
-                for (int j = 0; j < array.Length - 1; j++)
-                {
-                    array[j] = array[j + 1];
-                }
-
-                array[array.Length - 1] = tempNumber;
-            }
-
-            for (int i = 0; i < array.Length; i++)
-            {
-               Console.Write(array[i] + " ");
-            }
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
