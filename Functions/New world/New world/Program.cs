654efb8d0d199a5286a4b0fe19f280332b5d815a
ivane <ivan.ega4ev@yandex.ru>
2024-09-18 17:02:33 +0300

    logic of  movement in funftion

--- Functions/New world/New world/Program.cs
+++ Functions/New world/New world/Program.cs
@@ -35,8 +35,8 @@
         int playerDirectionX = 0;
         int playerDirectionY = 0;
 
-        int playerShiftX;
-        int playerShiftY;
+        int playerShiftX = 0;
+        int playerShiftY = 0;
 
         int coinsOnMapCount = GetItemsCount(map, coin);
         int collectedCoinCount = 0;
@@ -49,9 +49,7 @@
             if (Console.KeyAvailable)
             {
                 ChangeDirection(ref playerDirectionX, ref playerDirectionY);
-
-                playerShiftX = playerPositionX + playerDirectionX;
-                playerShiftY = playerPositionY + playerDirectionY;
+                ChangeShift(ref playerShiftX, ref playerShiftY, playerPositionX, playerDirectionX, playerPositionY, playerDirectionY);
 
                 if (map[playerShiftX, playerShiftY] != block)
                 {
@@ -63,8 +61,7 @@
                         map[playerShiftX, playerShiftY] = cleaner;
                     }
 
-                    playerPositionX += playerDirectionX;
-                    playerPositionY += playerDirectionY;
+                    ChangePosition(ref playerPositionX, ref playerPositionY, playerDirectionX, playerDirectionY);
 
                     OutputToConsole(player.ToString(), playerShiftY, playerShiftX);
                 }
@@ -73,8 +70,8 @@
             OutputToConsole("Соберите максимальное количеество монет за указанное время:", 0, 19);
             OutputToConsole($"Количество монет: {collectedCoinCount}/{coinsOnMapCount}", 0, 20);
 
-            delta = Update(delta, endGameTimer);
-            OutputToConsole($"Осталось {delta.Minutes.ToString("00")}:{delta.Seconds.ToString("00")}", 0, 21);
+            delta = Update(endGameTimer);
+            OutputToConsole($"Осталось {delta.Minutes:00}:{delta.Seconds:00}", 0, 21);
         }
 
         if (collectedCoinCount == coinsOnMapCount)
@@ -142,32 +139,50 @@
     {
         ConsoleKeyInfo key = Console.ReadKey(true);
 
+        const ConsoleKey up = ConsoleKey.UpArrow;
+        const ConsoleKey down = ConsoleKey.DownArrow;
+        const ConsoleKey left = ConsoleKey.LeftArrow;
+        const ConsoleKey right = ConsoleKey.RightArrow;
+
         playerDirectionX = 0;
         playerDirectionY = 0;
 
         switch (key.Key)
         {
-            case ConsoleKey.UpArrow:
+            case up:
                 playerDirectionX = -1;
                 break;
-            case ConsoleKey.DownArrow:
+
+            case down:
                 playerDirectionX = 1;
                 break;
-            case ConsoleKey.LeftArrow:
+
+            case left:
                 playerDirectionY = -1;
                 break;
-            case ConsoleKey.RightArrow:
+
+            case right:
                 playerDirectionY = 1;
-                break;
-            default:
                 break;
         }
     }
 
-    static TimeSpan Update(TimeSpan delta, DateTime endGameTimer)
+    static void ChangeShift(ref int playerShiftX, ref int playerShiftY, int playerPositionX, int playerDirectionX,
+                            int playerPositionY, int playerDirectionY)
     {
-        delta = endGameTimer - DateTime.Now;
-        return delta;
+        playerShiftX = playerPositionX + playerDirectionX;
+        playerShiftY = playerPositionY + playerDirectionY;
+    }
+
+    static void ChangePosition(ref int playerPositionX, ref int playerPositionY, int playerDirectionX, int playerDirectionY)
+    {
+        playerPositionX += playerDirectionX;
+        playerPositionY += playerDirectionY;
+    }
+
+    static TimeSpan Update(DateTime endGameTimer)
+    {
+        return endGameTimer - DateTime.Now;
     }
 
     static void OutputToConsole(string message, int positionY, int positionX)
